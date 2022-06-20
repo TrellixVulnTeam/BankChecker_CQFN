06f3289ba85ebd6442169a5e93ecadb86eb9c9da
federicoPezzopane <federico.pezzopane@yahoo.it>
2022-06-20 11:47:27 +0200

    Frontend

---
+++ Bankchecker Frontend/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AuthenticationService } from './service/authentication.service';
+import { UserService } from './service/user.service';
+import { ExpenseService } from './service/expense.service';
+import { AuthInterceptor } from './interceptor/auth.interceptor';
+import { NotificationService } from './service/notification.service';
+import { NotificationModule } from './notification.module';
+import { FormsModule } from '@angular/forms';
+import { InsertExpenseComponent } from './insert-expense/insert-expense.component';
+import { SearchExpensesComponent } from './search-expenses/search-expenses.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomepageComponent,
+    InsertExpenseComponent,
+    SearchExpensesComponent
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NotificationModule,
+    FormsModule,
+    
+   
+  ],
+  providers: [AuthenticationService, UserService, ExpenseService, NotificationService, {provide : HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
