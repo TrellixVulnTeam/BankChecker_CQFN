9903f411a91c319cc5239f2a5364e1147ee5e67b
federicoPezzopane <57091792+federicoPezzopane@users.noreply.github.com>
2022-07-14 16:52:50 +0200

    Delete Bankchecker Frontend directory

--- Bankchecker Frontend/src/app/service/user.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
-@Injectable({
-  providedIn: 'root'
-})
-export class UserService {
-  constructor() { }
-}
